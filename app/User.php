bb00341958f86d88dce231b045e2dec1f09bab09
laveena95 <laveenalavee256@gmail.com>
2020-07-06 14:45:51 +0530

    initial commit

--- app/User.php
+++ app/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
+    protected $guarded = [];
 
     /**
      * The attributes that are mass assignable.
--- webpack.mix.js
+++ webpack.mix.js
@@ -25,3 +25,9 @@ mix.webpackConfig( webpackConfig );
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+
+   /* mix.js('resources/assets/js/app.js', 'public/js/app.js')
+   .sass('resources/assets/sass/app.scss', 'public/css/app.css')
+   .version();
+*/
